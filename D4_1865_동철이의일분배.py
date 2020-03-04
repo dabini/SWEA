d223fa8d1ab28e22f4969c198bfe9d84cc851bb8
dabin Jo <jdb960211@gmail.com>
2020-03-04 14:09:59 +0900

    3월 4일

---
+++ D4_1865_동철이의일분배.py
@@ -0,0 +1,25 @@
+def f(n, k, s): #순열의 n 번 원소 결정
+    global maxV
+    if s <= maxV: #모든 일에 대해 사람이 정해지면, 이전의 최대 성공확률과 비교
+        return
+    if n == k:
+        if maxV < s: 
+            maxV = s
+        return
+
+    else:
+        for i in range(k):
+            if used[i] == 0:
+                used[i] = 1
+                f(n+1, k, s*board[i][n]/100)
+                used[i] = 0
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    used = [0] * N
+    p = [0] * N
+    maxV = -987654321
+    f(0, N, 1)
+    print("#%d %.6f"%(t+1, maxV*100))

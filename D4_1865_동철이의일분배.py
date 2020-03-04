bc01126e735959d29eb415e7e76677c4d887bd20
dabin Jo <jdb960211@gmail.com>
2020-03-04 23:50:35 +0900

    3월 4일

--- D4_1865_동철이의일분배.py
+++ D4_1865_동철이의일분배.py
@@ -3,7 +3,7 @@ def f(n, k, s): #순열의 n 번 원소 결정
     if s <= maxV: #모든 일에 대해 사람이 정해지면, 이전의 최대 성공확률과 비교
         return
     if n == k:
-        if maxV < s: 
+        if maxV < s:
             maxV = s
         return
 
@@ -20,6 +20,6 @@ for t in range(T):
     board = [list(map(int, input().split())) for _ in range(N)]
     used = [0] * N
     p = [0] * N
-    maxV = -987654321
+    maxV = 0
     f(0, N, 1)
     print("#%d %.6f"%(t+1, maxV*100))
---
+++ D4_2819 _격자판의 숫자 이어 붙이기.py
@@ -0,0 +1,4 @@
+T = int(input())
+for t in range(T):
+    board = [list(map(int, input().split())) for _ in range(4)]
+    print(board)

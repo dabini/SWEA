ee79b32c02f70ba14d40a4f38bcc76d9fa225fae
dabini <jdb960211@gmail.com>
2020-05-28 21:19:17 +0900

    5월 28일

--- D5_1247_최적경로.py
+++ D5_1247_최적경로.py
@@ -1,50 +1,30 @@
+def dfs(l, k , tmp):
+    global min_num
+    if k == N:
+        ans = tmp + abs(l[0] - lst[1][0]) + abs(l[1] - lst[1][1])
+        if ans < min_num:
+            min_num = ans
+            return
+
+    for i in range(N+2):
+        if not visited[i]:
+            ans = tmp + abs(l[0] - lst[i][0]) + abs(l[1] - lst[i][1])
+            if ans > min_num:
+                continue
+            visited[i] = 1
+            dfs(lst[i], k+1, ans)
+            visited[i] = 0
+
 T = int(input())
 for t in range(T):
-    cust_num = int(input())
+    N = int(input())
     loc = list(map(int, input().split()))
+    visited = [0] * (N +2)
+    visited[0] = visited[1] = 1
+    lst = []
+    min_num = 987654321
+    for i in range(0, N*2+4, 2):
+        lst.append([loc[i], loc[i+1]])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for T in range(1, int(input()) + 1):
-#     N = int(input())
-#     data = list(map(int, input().split()))
-#     visited = [0] * (N + 2)
-#     visited[0] = visited[1] = 1
-#     nodes = []
-#     mini = 99999999999
-#     for i in range(0, N * 2 + 4, 2):
-#         nodes.append([data[i], data[i + 1]])
-#
-#
-#     def dfs(node, depth, tot):
-#         global mini
-#         if depth == N:
-#             t = tot + abs(node[0] - nodes[1][0]) + abs(node[1] - nodes[1][1])
-#             if t < mini:
-#                 mini = t
-#                 return
-#         for i in range(N + 2):
-#             if not visited[i]:
-#                 t = tot + abs(node[0] - nodes[i][0]) + abs(node[1] - nodes[i][1])
-#                 if t > mini:
-#                     continue
-#                 visited[i] = 1
-#                 dfs(nodes[i], depth + 1, t)
-#                 visited[i] = 0
-#
-#
-#     dfs(nodes[0], 0, 0)
-#     print('#{} {}'.format(T, mini))
+    dfs(lst[0], 0, 0)
+    print("#{} {}".format(t+1, min_num))
